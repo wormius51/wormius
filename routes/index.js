edf52ef194557d5e7aad97fbab1d015fd9601de6
wormius51 <eyalhazor1@gmail.com>
2019-08-13 13:30:32 +0300

    bootstrap first try

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 router.get("/", (req, res, next) => {
-    res.render('index');
+    res.render('index', {title: "Home"});
+});
+
+router.get("/about", (req, res, next) => {
+    res.render('index', {title: "About"});
+});
+
+router.get("/games", (req, res, next) => {
+    res.render('index', {title: "Games"});
 });
 
 module.exports = router;
