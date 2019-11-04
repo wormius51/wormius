035234daad2311afa25fea67388ca9d808853c12
wormius51 <eyalhazor1@gmail.com>
2019-11-04 17:14:44 +0200

    burn-your-brain

--- routes/kongregate-test.js
+++ routes/kongregate-test.js
@@ -5,10 +5,12 @@ const apiKey = "a18df946-621d-4be2-9109-615783e9aca0";
 
 router.use("/getUser", (req, res) => {
     try {
+        console.log(req.headers.userid);
+        console.log(req.headers.gameauthtoken);
         https.get("https://api.kongregate.com/api/authenticate.json?user_id=" + req.headers.userid +
             "&game_auth_token=" + req.headers.gameauthtoken + "&api_key=" + apiKey,
             response => {
-                res.send(response.username);
+                res.send(response);
             });
     } catch (e) {
         res.send(e);
