bcc40d086e2b33d8b530f3fd5b2dce9b4941dcaf
wormius51 <eyalhazor1@gmail.com>
2019-11-04 12:53:37 +0200

    kongregate test

--- routes/kongregate-test.js
+++ routes/kongregate-test.js
@@ -4,6 +4,9 @@ const http = require('http');
 const apiKey = "a18df946-621d-4be2-9109-615783e9aca0";
 
 router.use("/getUser", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+    return;
     try {
         http.get("http://www.kongregate.com/api/authenticate.json?user_id=" + req.body.userId +
             "&game_auth_token=" + req.body.gameAuthToken + "&api_key=" + apiKey,
