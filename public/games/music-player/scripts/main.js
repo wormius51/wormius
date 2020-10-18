064cd378f84bbd48b5daa4c26af99db42054d799
wormius51 <eyalhazor1@gmail.com>
2020-10-18 20:10:54 +0300

    music-player

--- public/games/music-player/scripts/main.js
+++ public/games/music-player/scripts/main.js
@@ -1,3 +1,3 @@
 const musicTextField = document.getElementById("musicTextField");
 const playButton = document.getElementById("playButton");
-playButton.addEventListener('click', () => {playMusic(musicTextField.value)});
+playButton.addEventListener('click', () => {playMusic(musicTextField.value.replace("\n",""))});
