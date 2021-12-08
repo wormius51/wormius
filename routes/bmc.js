20a93b737e903aadcf501bc287830cf069f002d9
wormius51 <eyalhazor1@gmail.com>
2021-12-08 15:29:38 +0200

    supporters info

--- routes/bmc.js
+++ routes/bmc.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 
 router.get('/supporters', async (req, res) => {
-    if (process.env['bmc-token']) {
+    if (process.env.BMC_TOKEN) {
         const options = {
             method: 'get', 
-            headers: {'Authorization': `Bearer ${process.env['bmc-token']}`}
+            headers: {'Authorization': `Bearer ${process.env.BMC_TOKEN}`}
         }
         const bmcRes = await fetch('https://developers.buymeacoffee.com/api/v1/supporters', options);
         const body = await bmcRes.text();
         res.send(body);
     } else { // dummy data on local
+        console.log("no bmc token");
         res.send(`{
             "error": "no token" 
           }`);
