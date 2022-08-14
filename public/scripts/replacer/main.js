5741e02d54ac689b0f4c7f616cabd823283e4fda
wormius51 <eyalhazor1@gmail.com>
2022-08-14 12:37:13 +0200

    replacer page

---
+++ public/scripts/replacer/main.js
@@ -0,0 +1,5 @@
+
+const regexInput = document.getElementById('regexInput');
+const templateInput = document.getElementById('templateInput');
+const inputTextArea = document.getElementById('inputTextArea');
+const outputTextArea = document.getElementById('outputTextArea');
