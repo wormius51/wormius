9c386e7190b5ec6f61b94d57404546cf48a1d449
wormius51 <eyalhazor1@gmail.com>
2022-08-15 16:15:56 +0200

    \$

--- public/scripts/replacer/main.js
+++ public/scripts/replacer/main.js
@@ -1,5 +1,18 @@
+import { replaceTemplate } from "./replacer.js";
 
 const regexInput = document.getElementById('regexInput');
 const templateInput = document.getElementById('templateInput');
 const inputTextArea = document.getElementById('inputTextArea');
 const outputTextArea = document.getElementById('outputTextArea');
+const convertButton = document.getElementById('convertButton');
+
+
+convertButton.addEventListener('click', () => {
+    const output = replaceTemplate(
+        inputTextArea.value,
+        regexInput.value,
+        templateInput.value
+    );
+
+    outputTextArea.value = output;
+});
